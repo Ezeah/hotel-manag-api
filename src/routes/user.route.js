cab1293ce9be40d24a59572afb43fc7225944ce0
Ezeah <116434302+Ezeah@users.noreply.github.com>
2023-02-20 08:05:50 -0800

    Add files via upload

---
+++ src/routes/user.route.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const User = require("../models/user.model");
+
+const router = express.Router();
+
+// Route for user signup
+router.post("/signup", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    console.error(error);
+    res.status(400).send();
+  }
+});
+
+// Route for user login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password);
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (error) {
+    console.error(error);
+    res.status(400).send(error);
+  }
+});
+
+module.exports = router;
+
+
